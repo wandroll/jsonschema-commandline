4f1c6e03a7145dac55c3487ad53dcc3674549864
Wandrille Verlut <wandrille.verlut@gmail.com>
2017-03-15 23:24:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # jsonschema-commandline
+Based on [jsonschema](https://github.com/tdegrunt/jsonschema) validator, it is design to ease json validation from command line
+
+## Contributing & bugs
+Any improvment and feedbacks are welcome !
+
+
+## Usage
+Install dependencies with NPM or Yarn then 
+
+```javascript
+  node validator.js -s demo/person.schema.json demo/person.sample.json
---
+++ validator.js
@@ -0,0 +1,18 @@
+const Validator = require('jsonschema').Validator;
+const JSONReader = require('jsonfile');
+const Argv = require('yargs')
+            .usage('Usage: $0 -s jsonschema jsonsample')
+            .demandCommand(1)
+            .demandOption(['s'])
+            .argv;
+
+let samplePath = Argv._[0];
+let schemaPath = Argv.s;
+
+console.dir ('validationg %d against %d', samplePath, schemaPath);
+
+let sample = JSONReader.readFileSync(samplePath);
+let schema = JSONReader.readFileSync(schemaPath);
+
+var v = new Validator();
+console.log(v.validate(sample, schema));
